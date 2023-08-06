72b1c2495ae29c8091e2039512ba7b8db6f51e60
weizhixie <wx02697@yahoo.co.uk>
2023-08-06 20:55:14 +0100

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    else if (num < 1) return "";
+    else
+    {
+        let repeatString = string;
+        for(let i = 1; i < num; i++)
+        {
+            repeatString += string;
+        }
+        return repeatString;
+    }
 };
 
 // Do not edit below this line
