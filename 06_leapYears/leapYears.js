d4821343820a120e2b0f4ddb306d0659b1eef1a6
weizhixie <wx02697@yahoo.co.uk>
2023-08-07 22:07:35 +0100

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if ( Number.isInteger( year / 100 ) && !Number.isInteger( year / 400 ) ||
+     year % 4 !== 0 )
+    {
+        return false; 
+    }
+    
+    if (year % 4 === 0) return true;
 };
 
 // Do not edit below this line
