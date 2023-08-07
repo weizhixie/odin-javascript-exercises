bb1b2d923a270ce1deb72ae1cba99be5f16c86a8
weizhixie <wx02697@yahoo.co.uk>
2023-08-07 21:44:11 +0100

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(firstInt, secondInt) {
+    let sum = 0;
+    let smallInt = firstInt;
+    let largeInt = secondInt;
 
+    if ( firstInt < 0 || secondInt < 0 || 
+        !Number.isInteger(firstInt) || !Number.isInteger(secondInt) ) 
+    {
+        return 'ERROR';
+    }
+
+    if ( firstInt > secondInt)
+    {
+        largeInt = firstInt;
+        smallInt = secondInt
+    }
+
+    for (let i = smallInt; i <= largeInt; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
