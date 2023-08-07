9e7e968997e841775fc19f1facf58b42a2955cc0
weizhixie <wx02697@yahoo.co.uk>
2023-08-07 22:30:15 +0100

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const celsius = (fahrenheit - 32) * 5 / 9;
+  const roundedTemp = Math.round(celsius * 10) / 10;
+
+  return roundedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const fahrenheit = (celsius * 9 / 5) + 32;
+  const roundedTemp = Math.round(fahrenheit * 10) / 10;
+
+  return roundedTemp;
 };
 
 // Do not edit below this line
