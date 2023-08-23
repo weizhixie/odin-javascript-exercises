8ae746bd9eb36dfd47eede77addfc92f41e6aa49
weizhixie <wx02697@yahoo.co.uk>
2023-08-23 23:06:50 +0100

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,39 @@
-const add = function() {
-	
+const add = function (num1, num2) {
+    return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function (num1, num2) {
+    return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function (numArr) {
+    return numArr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numArr) {
+    return numArr.reduce((multiple, current) => multiple * current);
 };
 
-const power = function() {
-	
+const power = function (num1, num2) {
+    return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    let result = 1;
+
+    for (let i = num; i > 1; i--) {
+        result *= i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
