e6b6764187aa2d9687c387aa2a40d14bf3bd8d2b
weizhixie <wx02697@yahoo.co.uk>
2023-08-24 02:43:59 +0100

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const reversedStr = str.match(/[a-z0-9]/gi).reverse().join("").toLowerCase();
 
+    for (let i = 0; i < str.length; i++) {
+        if (str.replace(/[^a-z0-9]/gi, '').toLowerCase()[i] !== reversedStr[i]) {
+            return false;
+        }
+    }
+    return true;
+
+    // TOP github solution
+    // const processedString = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+    // return processedString.split("").reverse().join("") == processedString;
 };
 
 // Do not edit below this line
