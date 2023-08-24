d062647ec141f98d363a123398898a4c332864e5
weizhixie <wx02697@yahoo.co.uk>
2023-08-24 14:17:00 +0100

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.map(person => {
+        if (!person.yearOfDeath) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        return person;
+    }).sort((a, b) => (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth))[0];
 };
 
 // Do not edit below this line
