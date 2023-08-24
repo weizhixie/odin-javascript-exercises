a3a5a206ee9e003ca16d53203ce395eb72dc9878
weizhixie <wx02697@yahoo.co.uk>
2023-08-24 12:59:56 +0100

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = Number(num);
+    if (num === 0) return 0;
+    if (num === 1 || num === 2) return 1;
+    if (num < 0 || isNaN(num)) return "OOPS";
 
+    let preNum = 1;
+    let currentNum = 1;
+    let outputNum = 0;
+
+    for (let i = 3; i <= num; i++) {
+        outputNum = preNum + currentNum;
+        preNum = currentNum;
+        currentNum = outputNum;
+    }
+
+    return outputNum;
 };
 
 // Do not edit below this line
